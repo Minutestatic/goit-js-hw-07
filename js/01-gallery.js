b891689b7e68cee1b8507a0f21a438c7f38affde
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-04-06 02:12:35 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const cardGallery = createsGalleryPictures(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", cardGallery);
+
+gallery.addEventListener("click", onGalleryClick);
+
+function createsGalleryPictures(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+         </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function onGalleryClick(evt) {
+  console.log(evt);
+  evt.preventDefault();
+  if (!evt.target.dataset.source) {
+    return;
+  }
+
+  const originalImg = evt.target.dataset.source;
+  const description = evt.target.alt;
+
+  const instance = basicLightbox.create(`
+      <img src="${originalImg}" alt="${description}">
+  `);
+
+  instance.show();
+}
